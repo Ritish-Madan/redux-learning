de238fdd1d49bac708cc95fbc436b6a982c6e867
ritishmadan <work@ritish.me>
2023-08-27 01:10:28 +0530

    Dispatching the actions

--- src/index.js
+++ src/index.js
@@ -6,6 +6,12 @@ import { createStore } from 'redux';
 import { movies } from './reducers';
 
 const store = createStore(movies);
+console.log("Before State", store.getState())
+store.dispatch({
+  type: "ADD_MOVIES",
+  movies: [{name: "Superman"}]
+})
+console.log("After State", store.getState())
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
