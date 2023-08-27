cc2125599bf8ad10623ffbb25921d19203d70676
ritishmadan <work@ritish.me>
2023-08-27 12:27:14 +0530

    Subscribing to the store

--- src/components/App.js
+++ src/components/App.js
@@ -3,23 +3,39 @@ import {data} from "../data"
 import Navbar from "./Navbar";
 import MovieCard from "./MovieCard";
 
-function App() {
-  return( 
-  <div className="App">
-    <Navbar />
-    <div className="main">
-      <div className="tabs">
-        <div className="tab">Movies</div>
-        <div className="tab">Favourites</div>
+class App extends React.Component {
+  componentDidMount(){
+    console.log("Hey", this.props)
+    const {store} = this.props;
+
+    store.subscribe(() => {
+      console.log("Hey")
+      this.forceUpdate();
+    })
+
+    store.dispatch({
+      type: "ADD_MOVIES",
+      movies: {name: "Superman"}
+    })
+  }
+  render(){
+    return( 
+      <div className="App">
+        <Navbar />
+        <div className="main">
+          <div className="tabs">
+            <div className="tab">Movies</div>
+            <div className="tab">Favourites</div>
+          </div>
+          <div className="list">
+            {data.map((movie, index) => (
+              <MovieCard movie={movie} key={`movie-${index}`}/>
+            ))}
+          </div>
+        </div>
       </div>
-      <div className="list">
-        {data.map((movie, index) => (
-          <MovieCard movie={movie} key={`movie-${index}`}/>
-        ))}
-      </div>
-    </div>
-  </div>
-  )
+      )
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -6,16 +6,10 @@ import { createStore } from 'redux';
 import { movies } from './reducers';
 
 const store = createStore(movies);
-console.log("Before State", store.getState())
-store.dispatch({
-  type: "ADD_MOVIES",
-  movies: [{name: "Superman"}]
-})
-console.log("After State", store.getState())
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App store={store}/>
   </React.StrictMode>
 );
 
