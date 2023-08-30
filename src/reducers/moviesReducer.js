b0a57682a3672c51bbca36a1d5f20e011dc19e6b
ritishmadan <work@ritish.me>
2023-08-30 13:04:03 +0530

    Using combineReducers for multiple reducers

---
+++ src/reducers/moviesReducer.js
@@ -0,0 +1,37 @@
+import { ADD_MOVIES, ADD_FAVOURITES, REMOVE_FAVOURITES, SHOW_FAVOURITES } from "../actions";
+
+export const initialMoviesState = {
+    list:  [],
+    favourites: [],
+    showFavourites: false
+}
+
+export function movies(state = initialMoviesState, action){
+    switch(action.type){
+        case ADD_MOVIES:
+            return {
+                ...state,
+                list: action.movies
+            }
+        case ADD_FAVOURITES:
+            return{
+                ...state,
+                favourites: [action.movie, ...state.favourites]
+            }
+        case REMOVE_FAVOURITES:
+            const filteredArray = state.favourites.filter(
+                movie => movie.Title !== action.movie.Title
+            )
+            return {
+                ...state,
+                favourites: filteredArray
+            }         
+        case SHOW_FAVOURITES:
+            return {
+                ...state,
+                showFavourites: action.val
+            }
+        default:
+            return state;
+    }
+}
---
+++ src/reducers/searchReducer.js
@@ -0,0 +1,10 @@
+export const initialSearchState = {
+    searchResult:  []
+}
+
+export function search(state = initialSearchState, action){
+    switch(action.type){
+        default:
+            return state;
+    }
+}
