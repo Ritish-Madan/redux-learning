800555490e1f9eaaf3873f749c15ec9e20e9f6e8
ritishmadan <work@ritish.me>
2023-08-31 10:28:21 +0530

    Using the thunk redux's package for the same work

--- src/index.js
+++ src/index.js
@@ -4,19 +4,15 @@ import "./index.css";
 import App from "./components/App";
 import { applyMiddleware, createStore } from "redux";
 import rootReducer from "./reducers";
+import thunk from "redux-thunk";
 
 
 const logger = ({ dispatch, getState }) => (next) => (action) =>{
-  if(typeof (action) === 'function'){
-    console.log("HERE in function middleware action")
-    action(dispatch)
-    return;
-  }
   console.log("Hey MiddleWare Called ", action.type);
   next(action);
 }
 
-const store = createStore(rootReducer,   applyMiddleware(logger));
+const store = createStore(rootReducer, applyMiddleware(logger, thunk));
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
